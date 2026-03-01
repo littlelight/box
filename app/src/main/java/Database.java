7f7ee6cda39080e556cebf9bdd0136861c726e44
littlelight <littlelight1985@gmail.com>
2026-02-28 23:04:46 -0800

    implement other functions

--- app/src/main/java/Database.java
+++ app/src/main/java/Database.java
@@ -1,5 +1,6 @@
 import java.util.Map;
 import java.util.HashMap;
+import java.lang.Exception;
 
 public class Database {
     public enum DBStatus {
@@ -26,12 +27,21 @@ public class Database {
         return DBStatus.DB_GOOD;
     }
 
-    public DBStatus dbUnset(String key, int value) {
+    public DBStatus dbUnset(String key) {
+        if(decOldValue(key) == DBStatus.DB_NOT_FOUND) {
+            return DBStatus.DB_NOT_FOUND;
+        }
+
+        keyToValue.remove(key);
         return DBStatus.DB_GOOD;
     }
 
-    public DBStatus dbGet(String key, int value) {
-        return DBStatus.DB_GOOD;
+    public int dbGet(String key) {
+        if(!keyToValue.containsKey(key)) {
+            throw new Exception("key doesn't exists");
+        }
+
+        return keyToValue.get(key);
     }
 
     public DBStatus dbNumEqualTo(String key, int value) {
